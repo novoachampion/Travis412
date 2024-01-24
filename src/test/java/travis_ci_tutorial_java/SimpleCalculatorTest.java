53477b9d7ae199ced6206db2f41ef5d414e145e5
Novoa Champion <144487949+novoachampion@users.noreply.github.com>
2024-01-24 18:13:58 -0500

    wrote minus, multiply, and divde functions in test and class

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -16,4 +16,14 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(1, 1), 0);
 	}
+	@Test
+	public void testmultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(1, 1), 0);
+	}
+	@Test
+	public void testdivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(1, 1), 0);
+	}
 }
