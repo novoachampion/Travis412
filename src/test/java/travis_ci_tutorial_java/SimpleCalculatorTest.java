980a8cfd8e51e0b0e5a4c27a1d3facbf7af20a94
Novoa Champion <144487949+novoachampion@users.noreply.github.com>
2024-01-24 18:05:55 -0500

    adding a test for minus

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,9 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	@Test
+	public void testminus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(1, 1), 0);
+	}
 }
